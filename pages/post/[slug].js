07f032fcb4bd802befb2fe4263e108991be0ac63
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-01 17:40:46 +0600

    issue_fixed

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Author from '../../components/Author';
-import Categories from '../../components/Categorise';
-import Comments from '../../components/Comments';
-import CommentsForm from '../../components/CommentsForm';
-import PostDetail from '../../components/PostDetail';
-import PostWidget from '../../components/PostWidget';
-import { getPostDetails, getPosts } from '../../services';
+import { useRouter } from 'next/router';
 
-const PostDetails = ({post}) => {
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import { getPosts, getPostDetails } from '../../services';
+
+const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
   return (
-  <div className="container mx-auto px-10 mb-8">
+    <>
+      <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
-            {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
@@ -26,26 +29,27 @@ const PostDetails = ({post}) => {
           </div>
         </div>
       </div>
-  )
+    </>
+  );
 };
-
 export default PostDetails;
 
+// Fetch data at build time
 export async function getStaticProps({ params }) {
-    const data = await getPostDetails(params.slug);
-    return {
-      props: {
-        post: data,
-      },
-    };
-  }
-  
-  // Specify dynamic routes to pre-render pages based on data.
-  // The HTML is generated at build time and will be reused on each request.
-  export async function getStaticPaths() {
-    const posts = await getPosts();
-    return {
-      paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-      fallback: true,
-    };
-  }
+  const data = await getPostDetails(params.slug);
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
